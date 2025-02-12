0db94af8d80f2d604c6c1c8030975eece42f87a7
abdulahadkhan-RIT <143540374+abdulahadkhan-RIT@users.noreply.github.com>
2025-02-12 16:15:06 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
-
+terraform {
+  backend "s3" {
+    bucket         = "tfstatebucket-wp-github-activity"  
+    key            = "terraform.tfstate"           
+    region         = "us-east-1"                  
+  }
+}
 
 provider "aws" {
   region = var.aws_region
